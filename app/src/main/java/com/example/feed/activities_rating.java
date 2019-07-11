1295cb183286351e29a48798e82ac8a1ce6cb224
dinesh8892 <dineshsirvi143.ds@gmail.com>
2019-07-11 22:50:03 +0530

    feed layout

---
+++ app/src/main/java/com/example/feed/activities_rating.java
@@ -0,0 +1,14 @@
+package com.example.feed;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class activities_rating extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitiy_activities);
+    }
+}
---
+++ app/src/main/java/com/example/feed/not_enrolled_activity.java
@@ -0,0 +1,25 @@
+package com.example.feed;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class not_enrolled_activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not_enrolled);
+
+    }
+}
